40f6c97c2e165082b0a69f3a238d3b2ebba5cb40
KonstantinYev <109310447+KonstantinYevchuk@users.noreply.github.com>
2023-08-21 21:01:02 +0300

    add routes, pages with contacts and posts, add api

--- src/pages/contacts/index.js
+++ src/pages/contacts/index.js
@@ -1,14 +1,40 @@
-import { useEffect, useState } from "react";
 import Head from "next/head";
+import Link from "next/link";
 import Heading from "@/components/Heading";
 
-const Contacts = () => (
+export const getStaticProps = async () => {
+      const response = await fetch('https://jsonplaceholder.typicode.com/users');
+      const data = await response.json();
+
+      if (!data) {
+        return {
+          notFound: true,
+        }
+      }
+      
+      return {
+        props: {contacts: data},
+      }
+}
+
+const Contacts = ({contacts}) => {
+ 
+  return (
     <>
-     <Head>
-        <title>Contacts</title>
-      </Head>
-    <Heading text ="Contacts list:" />
-    </>
+   <Head>
+      <title>Contacts</title>
+    </Head>
+  <Heading text ="Contacts list:" />
+  <ul>
+    {contacts && contacts.map(({id, name}) => (
+      <li key={id}>
+        <Link href={`/contacts/${id}`}>{name}</Link>
+      </li>
+    ))}
+  </ul>
+  </>
   )
+};
+    
 
 export default Contacts
